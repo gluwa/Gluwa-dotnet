ee6f54eb0573ac88b60960eca62ba592a287f795
kyeongmin.kang <kyeongmin.kang@gluwa.com>
2020-05-11 19:59:58 +0900

    Edit Webhook PayLoad

---
+++ SDK_dotnet/Models/Data.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gluwa.SDK_dotnet.Models
+{
+    public sealed class Data
+    {
+        /// <summary>
+        /// Transaction amount.
+        /// </summary>
+        public string Amount { get; set; }
+
+        /// <summary>
+        /// TransactionConfirmed or TransactionCreated or TransactionFailed
+        /// </summary>
+        [Required]
+        public EEventType? EventType { get; set; }
+
+        /// <summary>
+        /// Webhook
+        /// </summary>
+        [Required]
+        public ENotificationType? Type { get; set; }
+
+        /// <summary>
+        /// Transcation ID
+        /// </summary>
+        public string ResourceID { get; set; }
+    }
+}
