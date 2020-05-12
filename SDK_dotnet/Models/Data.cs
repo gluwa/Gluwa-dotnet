19fcd82f53604ee2959b28e7abc1172c6974fcfd
kyeongminkang <50939818+kyeongminkang@users.noreply.github.com>
2020-05-12 16:46:43 +0900

    Edit Webhook PayLoad (#5)
    * Edit Webhook PayLoad
    * Add WebhookPayLoad Property

---
+++ SDK_dotnet/Models/Data.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gluwa.SDK_dotnet.Models
+{
+    public sealed class Data
+    {
+        /// <summary>
+        /// Identifier for the transaction that was provided by the merchant user.
+        /// </summary>
+        public string MerchantOrderID { get; set; }
+
+        /// <summary>
+        /// Transaction amount.
+        /// </summary>
+        public string Amount { get; set; }
+
+        /// <summary>
+        /// TransactionConfirmed or TransactionCreated or TransactionFailed
+        /// </summary>
+        [Required]
+        public EEventType? EventType { get; set; }
+
+        /// <summary>
+        /// Webhook
+        /// </summary>
+        [Required]
+        public ENotificationType? Type { get; set; }
+
+        /// <summary>
+        /// Transcation ID
+        /// </summary>
+        public string ResourceID { get; set; }
+    }
+}
--- SDK_dotnet/Models/PayLoad.cs
+++ SDK_dotnet/Models/PayLoad.cs
@@ -1,29 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Gluwa.SDK_dotnet.Models
+﻿namespace Gluwa.SDK_dotnet.Models
 {
     public sealed class PayLoad
     {
-        /// <summary>
-        /// Identifier for the transaction that was provided by the merchant user.
-        /// </summary>
-        public string MerchantOrderID { get; set; }
-
-        /// <summary>
-        /// TransactionConfirmed or TransactionCreated or TransactionFailed
-        /// </summary>
-        [Required]
-        public EEventType? EventType { get; set; }
-
-        /// <summary>
-        /// Webhook
-        /// </summary>
-        [Required]
-        public ENotificationType? Type { get; set; }
-
-        /// <summary>
-        /// Transcation ID
-        /// </summary>
-        public string ResourceID { get; set; }
+        public Data Data { get; set; }
     }
 }
