5ce56ecd193d5405916eb8a88455b206026e1a06
kyeongmin.kang <kyeongmin.kang@gluwa.com>
2020-10-20 18:54:10 +0900

    Support Webhook V2

---
+++ SDK_dotnet/Models/EEventName.cs
@@ -0,0 +1,9 @@
+﻿namespace Gluwa.SDK_dotnet.Models
+{
+    public static class EEventName
+    {
+        public const string TRANSACTION_CONFIRMED = "TRANSACTION.CONFIRMED";
+        public const string TRANSACTION_CREATED = "TRANSACTION.CREATED";
+        public const string TRANSACTION_FAILED = "TRANSACTION.FAILED";
+    }
+}
